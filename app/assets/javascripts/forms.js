df241e71e007d4e0754971cc749a4744dc11b224
Adam Fortuna <adam@fortuna.name>
2011-10-01 09:20:18 -0500

    Updated some js files.

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,8 +1,3 @@
-//= require jquery
-//= require jquery.mobile
-//= require rails
-//= require forms
-
 $(function() {
   $(".dish_check_form input").live('change',  function(e) {
     if(!$(e.target).data('submitting')) {
@@ -10,13 +5,13 @@ $(function() {
       $(e.target).parent().submit();
     }
   });
-  
+
   $(".dish_order select").live('change', function(e) {
     if(!$(e.target).data('submitting')) {
       $(e.target).data('submitting', true);
       $(e.target).parent().submit();
     }
   });
-  
+
   $('select').selectmenu();
 })
